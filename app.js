b7c106bfdbfb6887a6ecc3d220a1d91c6628243f
Eduardo Zaghi <eduardozaghi@gmail.com>
2015-01-27 19:37:47 -0200

    refectory to use underscore lib

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// underscore lib
+var _ = require('underscore');
+
 // Setup basic express server
 var express = require('express.io');
 var app     = express('localhost');
@@ -28,36 +31,18 @@ var puzzleArray = [
 
 var puzzleManager = function(puzzleArray){
   var _this         = this;
-  this.puzzleArray  = puzzleArray;
+  this.puzzleArray  = _.shuffle(puzzleArray);
   this.clientsArray = {};
 
-  this.startup = function(){
-    this.shufflePuzzle();
-  },
+  this.getPuzzleCard = function(socketId, screenNumber){
+    var puzzleCard = _.findWhere(this.puzzleArray, {taken: false});
+    
+    puzzleCard.taken            = true;
+    puzzleCard.index            = screenNumber - 1;
+    puzzleCard.positioned       = puzzleCard.index === puzzleCard.rightPosition;
+    this.clientsArray[socketId] = puzzleCard;
 
-  this.shufflePuzzle = function() {
-    for (var i = this.puzzleArray.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = this.puzzleArray[i];
-        this.puzzleArray[i] = this.puzzleArray[j];
-        this.puzzleArray[j] = temp;
-
-        if(this.puzzleArray[i].rightPosition !== i){ this.puzzleArray[i].positioned = false; }
-        if(this.puzzleArray[j].rightPosition !== j){ this.puzzleArray[j].positioned = false; }
-    }
-    return this.puzzleArray;
-  }
-
-  this.getPuzzleCard = function(socketId){
-    for (var i = 0; i < this.puzzleArray.length; i++) {
-      if(this.puzzleArray[i].taken){ continue; }
-
-      this.puzzleArray[i].taken   = true;
-      this.clientsArray[socketId] = this.puzzleArray[i];
-      this.clientsArray[socketId].index = i;
-
-      return this.puzzleArray[i];
-    };
+    return puzzleCard;
   },
 
   this.changePuzzleCards = function(socketIdHolding, socketIdClicked){
@@ -86,15 +71,8 @@ var puzzleManager = function(puzzleArray){
   },
 
   this.isPuzzleCompleted = function(){
-    for (var i = 0; i < this.puzzleArray.length; i++) {
-      if(this.puzzleArray[i].positioned === false){ 
-        return false; 
-      }
-    }
-    return true;
+    return _.findWhere(this.puzzleArray, {positioned: false}) === undefined;
   }
-
-  this.startup();
 }
 
 var pM      = new puzzleManager(puzzleArray);
@@ -102,18 +80,12 @@ var pM      = new puzzleManager(puzzleArray);
 var activeUser;
 var numUsers = 0;
 
-app.io.route('ready', function(req) {
-    req.io.emit('talk', {
-        message: 'io event from an io route on the server'
-    })
-})
-
 app.io.route('connected', function(req) {
   ++numUsers;
 
   console.log('[NEW USER] - We have %d users connected', numUsers);
 
-  req.socket.emit('welcome', pM.getPuzzleCard(req.socket.id));
+  req.socket.emit('welcome', pM.getPuzzleCard(req.socket.id, numUsers));
 });
 
 app.io.route('click', function(req) {
@@ -121,6 +93,11 @@ app.io.route('click', function(req) {
     pM.changePuzzleCards(activeUser, req.socket.id);
     req.io.emit('change', pM.clientsArray[req.socket.id]);
     app.io.sockets.socket(activeUser).emit('change', pM.clientsArray[activeUser]);
+    // app.io.broadcast('change', pM.clientsArray[req.socket.id])
+    // app.io.broadcast('change', pM.clientsArray[activeUser])
+    if(pM.isPuzzleCompleted()){
+      console.log('COMPLETO!!!!')
+    }
 
     activeUser = null;
   }else{
