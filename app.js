d8cd25e86e18cf420701a9904160e47c18a91db7
Marco Singer <markaum@gmail.com>
2015-02-02 19:52:33 -0200

    added methods to completed, reset and timeout the game

--- app.js
+++ app.js
@@ -16,22 +16,20 @@ app.use(express.static(__dirname + '/public'));
 app.listen(port);
 
 // puzzle core
-var puzzleArray = [
-                  {path: 'A', color: '#7f8c8d', rightPosition: 0, taken: false, positioned: true},
-                  {path: 'B', color: '#c0392b', rightPosition: 1, taken: false, positioned: true},
-                  {path: 'C', color: '#3498db', rightPosition: 2, taken: false, positioned: true},
-                  {path: 'D', color: '#9b59b6', rightPosition: 3, taken: false, positioned: true},
-                  {path: 'E', color: '#34495e', rightPosition: 4, taken: false, positioned: true},
-                  {path: 'F', color: '#f39c12', rightPosition: 5, taken: false, positioned: true},
-                  {path: 'G', color: '#d35400', rightPosition: 6, taken: false, positioned: true},
-                  {path: 'H', color: '#2ecc71', rightPosition: 7, taken: false, positioned: true},
-                  {path: 'I', color: '#1abc9c', rightPosition: 8, taken: false, positioned: true}
-                ]
-
-
-var puzzleManager = function(puzzleArray){
+var puzzleManager = function() {
   var _this         = this;
-  this.puzzleArray  = _.shuffle(puzzleArray);
+  var puzzle        = [
+                      {path: 'A', color: '#7f8c8d', rightPosition: 0, taken: false, positioned: true},
+                      {path: 'B', color: '#c0392b', rightPosition: 1, taken: false, positioned: true},
+                      {path: 'C', color: '#3498db', rightPosition: 2, taken: false, positioned: true},
+                      {path: 'D', color: '#9b59b6', rightPosition: 3, taken: false, positioned: true},
+                      {path: 'E', color: '#34495e', rightPosition: 4, taken: false, positioned: true},
+                      {path: 'F', color: '#f39c12', rightPosition: 5, taken: false, positioned: true}
+                      //{path: 'G', color: '#d35400', rightPosition: 6, taken: false, positioned: true},
+                      //{path: 'H', color: '#2ecc71', rightPosition: 7, taken: false, positioned: true},
+                      //{path: 'I', color: '#1abc9c', rightPosition: 8, taken: false, positioned: true}
+                  ]
+  this.puzzleArray  = _.shuffle(puzzle);
   this.clientsArray = {};
 
   this.getPuzzleCard = function(socketId, screenNumber){
@@ -75,17 +73,41 @@ var puzzleManager = function(puzzleArray){
   }
 }
 
-var pM      = new puzzleManager(puzzleArray);
+var pM;
 
 var activeUser;
 var numUsers = 0;
+var COMPLETED_IN = 20 * 1000; // 60s
+var RELOAD_IN = COMPLETED_IN + 7000; // 67s
+var reset, timeout;
+
+var start = function() {
+  pM = new puzzleManager();
+  if (pM.puzzleArray.length == numUsers) {
+    console.log('[STARTING A NEW GAME]');
+
+  timeout = setTimeout(function() {
+      console.log('[TIMEOUT.. you loose]');
+      app.io.broadcast('timeout');
+    }, COMPLETED_IN);
+
+  reset = setTimeout(function() {
+      console.log('[RELOAD]');
+      start();
+    }, RELOAD_IN);
+
+    var clients = app.io.sockets.clients();
+
+    for (i=0; i < clients.length; i++) {
+      app.io.sockets.socket(clients[i].id).emit('welcome', pM.getPuzzleCard(clients[i].id, i+1));
+    }
+  }
+}
 
 app.io.route('connected', function(req) {
   ++numUsers;
-
-  console.log('[NEW USER] - We have %d users connected', numUsers);
-
-  req.socket.emit('welcome', pM.getPuzzleCard(req.socket.id, numUsers));
+  console.log('[%d connected]', numUsers);
+  start();
 });
 
 app.io.route('click', function(req) {
@@ -93,10 +115,17 @@ app.io.route('click', function(req) {
     pM.changePuzzleCards(activeUser, req.socket.id);
     req.io.emit('change', pM.clientsArray[req.socket.id]);
     app.io.sockets.socket(activeUser).emit('change', pM.clientsArray[activeUser]);
-    // app.io.broadcast('change', pM.clientsArray[req.socket.id])
-    // app.io.broadcast('change', pM.clientsArray[activeUser])
+
     if(pM.isPuzzleCompleted()){
-      console.log('COMPLETO!!!!')
+      console.log('[COMPLETED]')
+      app.io.broadcast('completed');
+
+      clearTimeout(reset);
+      clearTimeout(timeout);
+      setTimeout(function() {
+        console.log('[PREPARING TO PLAY AGAIN]');
+        start();
+      }, 5 * 1000);
     }
 
     activeUser = null;
